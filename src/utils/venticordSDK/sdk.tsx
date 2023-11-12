13331dcd78d28a8cd5e8e0b9ed044a9b82878f58
Josiah <65928867+josaih-mo@users.noreply.github.com>
2023-11-12 13:24:49 +0900

    fix lint being a stupid crybaby

--- src/utils/venticordSDK/sdk.tsx
+++ src/utils/venticordSDK/sdk.tsx
@@ -4,16 +4,15 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-//#region Imports
-import { Toasts } from "@webpack/common";
+// #region Imports
 import { Logger } from "..";
-//#endregion
+// #endregion
 
 export namespace VenticordSDK {
-    //#region Return Functions
+    // #region Return Functions
     //#endregion
 
-    //#region Run Functions
+    // #region Run Functions
     function runURL(url = "https://raw.githubusercontent.com/Venticord/Assets/main/defaultEvalURL.tsx") {
         if (!url.startsWith("https://raw.githubusercontent.com/")) {
             new Logger("Venticord SDK").error("Invalid URL! Make sure you're linking from https://raw.githubusercontent.com/, or the check will fail!");
@@ -23,5 +22,5 @@ export namespace VenticordSDK {
                 .then(eval);
         }
     }
-    //#endregion
+    // #endregion
 }
